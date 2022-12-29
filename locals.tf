706639eed885c68e1b243fdcce17900d9fe66093
toberosa <tobe.pl@gmail.com>
2022-12-29 11:59:33 +0800

    EKS Kubernetes - demo

---
+++ locals.tf
@@ -0,0 +1,36 @@
+locals {
+
+  name            = basename(path.cwd)
+  region          = data.aws_region.current.name
+  cluster_version = "1.23"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  node_group_name = "managed-ondemand"
+
+  #---------------------------------------------------------------
+  # ARGOCD ADD-ON APPLICATION
+  #---------------------------------------------------------------
+
+  addon_application = {
+    path               = "chart"
+    repo_url           = "https://github.com/toberosa/eks-blueprints-add-ons.git"
+    add_on_application = true
+  }
+
+  #---------------------------------------------------------------
+  # ARGOCD WORKLOAD APPLICATION
+  #---------------------------------------------------------------
+
+  workload_application = {
+    path               = "envs/dev"
+    repo_url           = "https://github.com/toberosa/eks-blueprints-workloads.git"
+    add_on_application = true
+  }
+
+  tags = {
+    Blueprint  = local.name
+    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+  }
+}
